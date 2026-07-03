0b9abcf94464c398685f9066cd9da1fae63f366a
SCBProd <scbproduktion@gmail.com>
2026-07-03 18:17:37 +0200

    avatar-+change

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,13 +2,22 @@
 import { Router } from 'express';
 import { getCurrentUser , updateAvatar } from '../controllers/profileController.js';
 import { authenticate } from '../middleware/authenticate.js';
-const profileRoutes = Router();
+import {upload} from '../middleware/multer.js';
 
+const profileRoutes = Router();
 profileRoutes.get('/current', authenticate, getCurrentUser);
-profileRoutes.patch('/avatar', authenticate, updateAvatar);
+profileRoutes.patch(
+  '/avatar',
+  authenticate,
+  upload.single('avatar'),
+  updateAvatar
+);
 
 
 
 export default profileRoutes;
+
+
+
 
 
