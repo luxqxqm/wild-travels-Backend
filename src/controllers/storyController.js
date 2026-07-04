f9eb9a6275568ff09c3c622c60508fc74374b3d2
ccnvxii <dasharossoha16@gmail.com>
2026-07-04 21:29:08 +0300

    refactor: restructure story retrieval methods for better clarity

--- src/controllers/storyController.js
+++ src/controllers/storyController.js
@@ -1,18 +1,46 @@
-import * as storyService from '../services/storyService.js';
 import createHttpError from 'http-errors';
+import { Story } from '../models/story.js';
 
-export const getStoryById = async (req, res, next) => {
-  try {
-    const { storyId } = req.params;
+export const getOwnStories = async (req, res) => {
+  const { page = 1, perPage = 10 } = req.query;
 
-    const story = await storyService.getStoryById(storyId);
+  const skip = (page - 1) * perPage;
 
-    if (!story) {
-      throw createHttpError(404, 'Така історія відсутня');
-    }
+  const storyQuery = Story.find({
+    ownerId: req.user._id,
+  });
 
-    return res.status(200).json(story);
-  } catch (error) {
-    next(error);
+  const [totalItems, stories] = await Promise.all([
+    storyQuery.clone().countDocuments(),
+    storyQuery.skip(skip).limit(Number(perPage)).sort({ createdAt: -1 }),
+  ]);
+
+  res.status(200).json({
+    page: Number(page),
+    perPage: Number(perPage),
+    totalItems,
+    totalPages: Math.ceil(totalItems / perPage),
+    stories,
+  });
+};
+
+export const getOwnStoriByID = async (req, res) => {
+  const { storyId } = req.params;
+  const story = await Story.findOne({
+    _id: storyId,
+    ownerId: req.user._id,
+  });
+  if (!story) {
+    throw createHttpError(404, ' Story not found');
   }
+  res.status(200).json(story);
+};
+
+export const getStoryById = async (req, res) => {
+  const { storyId } = req.params;
+  const story = await Story.findById(storyId);
+  if (!story) {
+    throw createHttpError(404, 'Story not found');
+  }
+  res.status(200).json(story);
 };
