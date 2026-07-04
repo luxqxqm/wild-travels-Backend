b8cec00df8762facd1036c3d380f1009f897c869
ccnvxii <dasharossoha16@gmail.com>
2026-07-04 21:21:33 +0300

    style: format story schema for improved readability

--- src/models/story.js
+++ src/models/story.js
@@ -1,14 +1,37 @@
-import { model, Schema } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const storySchema = new Schema(
   {
-    img: { type: String, required: true },
-    title: { type: String, required: true },
-    article: { type: String, required: true },
-    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-    rate: { type: Number, default: 0 },
-    ownerId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    date: { type: String, required: true },
+    img: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    article: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'category',
+      required: true,
+    },
+    rate: {
+      type: Number,
+      default: 0,
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
   },
   { versionKey: false, timestamps: true },
 );
