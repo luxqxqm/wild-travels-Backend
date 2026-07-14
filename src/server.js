295e2783edadf2e3925d3f6d650fcc44b288046e
Den Yaseniuk <denasenuk@gmail.com>
2026-07-14 21:03:56 +0200

    edit cors

--- src/server.js
+++ src/server.js
@@ -1,39 +1,44 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import swaggerUi from 'swagger-ui-express';
-import { errors } from 'celebrate';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import { errors } from "celebrate";
 
-import { logger } from './middleware/logger.js';
-import { connectMongoDB } from './db/connectMongoDB.js';
-import { notFoundHandler } from './middleware/notFoundHandler.js';
-import { errorHandler } from './middleware/errorHandler.js';
-import { swaggerSpec } from './docs/swagger.js';
+import { logger } from "./middleware/logger.js";
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { swaggerSpec } from "./docs/swagger.js";
 
-import authRoutes from './routes/authRoutes.js';
-import storiesRoutes from './routes/stories.js';
-import userRoutes from './routes/usersRoutes.js';
-import profileRoutes from './routes/profile.js';
-import categoryRoutes from './routes/categories.js';
+import authRoutes from "./routes/authRoutes.js";
+import storiesRoutes from "./routes/stories.js";
+import userRoutes from "./routes/usersRoutes.js";
+import profileRoutes from "./routes/profile.js";
+import categoryRoutes from "./routes/categories.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://wild-travels.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(helmet());
 app.use(cookieParser());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/stories', storiesRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/categories', categoryRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/stories", storiesRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/categories", categoryRoutes);
 
 app.use(notFoundHandler);
 
